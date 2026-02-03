1bfe289e8afa0fc3fa792094654cb363ea26194f
larealban25-ing <larealban173@gmail.com>
2026-02-03 21:57:51 +0100

    Fix: utiliser des chemins relatifs dans les tests

--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -1,5 +1,6 @@
+import os
 import sys
-sys.path.insert(0, 'c:\\Users\\larea\\Documents\\Projet_Signal')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.ecg_processing.io import synthetic_ecg
 import matplotlib.pyplot as plt
